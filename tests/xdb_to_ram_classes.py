8f81fd15307bca64666f5f63b447a1ad61e33968
VolodinE <E-O-V-95@yandex.ru>
2019-02-15 17:10:39 +0300

    domain_test_add

--- tests/xdb_to_ram_classes.py
+++ tests/xdb_to_ram_classes.py
@@ -3,57 +3,71 @@
 from collections import defaultdict
 #from collections import
 
+
+
 attribute = 0
 value = 1
+
+class primare_key:
+    def __init__(self,id,type):             # id - значение PK,
+        self.id = id                        #используем  как ключ:
+        self.type = type                    #имя для домена
+    def __eq__(self, other):                #имя для таблицы
+        if (self.id == other.id):
+            return True
+        return False
+
+    def pk(self):
+        return self.id
+
+
 
 from xml.dom.minidom import parse
 class schema:
 
     def __init__(self,xdb_file):
         self.xdb_file = parse(xdb_file)
-
         self.domains = self.get_domains()
 
     def __eq__(self, other):
-        for i in self.domains:
-            print(i)
-
-
-        input()
-
-        print(other.domains)
-        input()
-
         if (self.domains == other.domains):
             return True
         else:
             return False
 
     def get_domains(self):
-
-      #  domains_set = list()
         domains_set = set()
         domains = self.xdb_file.getElementsByTagName("domain")
+
         for domain_description in domains:
 
-            domain_dict = defaultdict()
-            for item in (domain_description.attributes.items()):
-                #       domain_dict[item[attribute]]=item[value]
-                domain_dict[item[attribute]] = item[value]
-
-            domains_set.add(tuple(domain_dict.items()))
-            #domains_set.update((domain.attributes.items()))
-
+            domains_set.add(domain(domain_description.attributes.items()))
         return domains_set
 
 class domain:
     def __init__(self,items):
+        self.primary_key = primare_key(items[0][1],"domain")
+        #self.name = items[0][1]
         self.description = dict()
-        for item in items:
-            self.description[item]=item
 
+        for item in items:
+            self.description[item[0]]=item[1]
+        self.description = (self.description)
     def description(self):
-        return self.description.items()
+        return self.description
+
+
+    def __hash__(self):
+        return hash(self.primary_key.pk())
+
+    def __eq__(self, other):
+      #  is_Equal = True
+#        if (set.difference(self.description,other.description)):
+        if self.description==other.description:
+            if self.primary_key.pk()==other.primary_key.pk():
+                return True
+                #прописать исключение для равенства значений и неравенства ключей
+        return False
 
 schema1 = schema("tasks.xdb")
 schema2 = schema("tasks.xdb")
