045b54868049c5ee365b3cab1c28b6ed0bd3a536
VolodinE <E-O-V-95@yandex.ru>
2019-02-15 20:40:38 +0300

    field_to_ram_1

--- tests/xdb_to_ram.py
+++ tests/xdb_to_ram.py
@@ -1,1 +1,1 @@
-#
+def test_fields(fields_1,fields_2):
--- tests/xdb_to_ram_classes.py
+++ tests/xdb_to_ram_classes.py
@@ -3,23 +3,33 @@
 from collections import defaultdict
 #from collections import
 
-
-
 attribute = 0
 value = 1
 
 class primare_key:
-    def __init__(self,id,type):             # id - значение PK,
-        self.id = id                        #используем  как ключ:
-        self.type = type                    #имя для домена
-    def __eq__(self, other):                #имя для таблицы
-        if (self.id == other.id):
-            return True
-        return False
+    types = set()
+    num_of_string_of_type = defaultdict(int)
+    def __init__(self,type):
+        self.id = primare_key.num_of_string_of_type[type]
+        self.type = type
+        primare_key.types.add(type)
+        primare_key.num_of_string_of_type[type]+=1          # id - значение PK,
+                                                            #используем  как ключ:
+                                                            #имя для домена
+    def __eq__(self, other):                                #имя для таблицы
+       # if (self.type == self.type):                    #int >=0 для поля
+       #     return True
+       # return False
+        return True
+
+    def __hash__(self):
+        return hash(self.type)
 
     def pk(self):
-        return self.id
+        return (self.id, self.type)
 
+  #  def pk_of_pk(self):
+  #      return self.pk_id
 
 
 from xml.dom.minidom import parse
@@ -27,10 +37,12 @@ class schema:
 
     def __init__(self,xdb_file):
         self.xdb_file = parse(xdb_file)
-        self.domains = self.get_domains()
+        self.domains = frozenset(self.get_domains())
+        self.fields = frozenset(self.get_fields())
 
     def __eq__(self, other):
-        if (self.domains == other.domains):
+        if (self.domains == other.domains
+            and self.fields == other.fields):
             return True
         else:
             return False
@@ -38,39 +50,64 @@ class schema:
     def get_domains(self):
         domains_set = set()
         domains = self.xdb_file.getElementsByTagName("domain")
-
         for domain_description in domains:
-
             domains_set.add(domain(domain_description.attributes.items()))
         return domains_set
 
+    def get_fields(self):
+        fields_set = set()
+        fields = self.xdb_file.getElementsByTagName("field")
+        for field_description in fields:
+            fields_set.add(field(field_description.attributes.items()))
+        return fields_set
+
 class domain:
     def __init__(self,items):
-        self.primary_key = primare_key(items[0][1],"domain")
-        #self.name = items[0][1]
+        self.primary_key = primare_key("domain")
         self.description = dict()
-
         for item in items:
             self.description[item[0]]=item[1]
-        self.description = (self.description)
+        self.description = self.description
     def description(self):
         return self.description
 
 
     def __hash__(self):
-        return hash(self.primary_key.pk())
+        return hash(tuple(self.description,))
 
     def __eq__(self, other):
       #  is_Equal = True
 #        if (set.difference(self.description,other.description)):
         if self.description==other.description:
-            if self.primary_key.pk()==other.primary_key.pk():
-                return True
+            return True
                 #прописать исключение для равенства значений и неравенства ключей
         return False
 
+class field:
+    def __init__(self,items):
+        self.pk = primare_key("field")
+        self.description = dict()
+        for item in items:
+            self.description[item[0]]=item[1:]
+
+    def __hash__(self):
+        return hash(tuple(self.description,))
+
+    def __eq__(self, other):
+        if (#self.table_id == other.table_id
+            self.description == other.description):
+            return True
+        else:
+            return False
+
+    def print_field(self):
+        print(self.description)
+
+
 schema1 = schema("tasks.xdb")
 schema2 = schema("tasks.xdb")
+
+#print(schema1.print_fields())
 
 print(schema1==schema2)
 
